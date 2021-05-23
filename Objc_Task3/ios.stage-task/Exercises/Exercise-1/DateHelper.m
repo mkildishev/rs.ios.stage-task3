62f7771e6168d52e4d178e7761c29c6ef8f57e57
Maksim Kildishev <mkildishev@yandex.ru>
2021-05-24 01:40:12 +0300

    add: implement first and second tasks

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,25 +5,49 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    if (monthNumber <= 0 || monthNumber > 12) {
+        return nil;
+    }
+    NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+    return [[formatter monthSymbols] objectAtIndex:(monthNumber - 1)];
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+    NSString* dateFormat1 = @"yyyy-MM-dd'T'HH:mm:ssZ";
+    [formatter setDateFormat:dateFormat1];
+    NSDate* dateFromString = [formatter dateFromString:date];
+    if (dateFromString == nil) {
+        return 0;
+    }
+    NSCalendar* calendar = [NSCalendar autoupdatingCurrentCalendar];
+    [calendar setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
+    return [calendar component:NSCalendarUnitDay fromDate:dateFromString];
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    NSCalendar* calendar = [NSCalendar autoupdatingCurrentCalendar];
+    [calendar setTimeZone:[NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
+    NSLocale* locale = [[NSLocale alloc] initWithLocaleIdentifier:@"ru_RU"];
+    [calendar setLocale:locale];
+    NSInteger weekDayIdx = [calendar component:NSCalendarUnitWeekday fromDate:date];
+    if (weekDayIdx == NULL) {
+        return nil;
+    }
+    return [calendar shortWeekdaySymbols][weekDayIdx];
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
-    return NO;
+    NSDate* nowDate = [NSDate now];
+    NSCalendar* calendar = [NSCalendar autoupdatingCurrentCalendar];
+    return [calendar component:NSCalendarUnitWeekOfMonth fromDate:date] ==
+    [calendar component:NSCalendarUnitWeekOfMonth fromDate:nowDate];
 }
 
 @end
--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,33 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    NSInteger result =[self dividePlayersIntoTeams:ratingArray team:@[]];
+    NSLog(@"RESULT: %lu", (long)result);
+    return result;
+}
+
+- (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *> *)ratingArray
+                               team:(NSArray<NSNumber *> *)teamRatingArray  {
+    NSInteger teamFormedCount = 0;
+    if (teamRatingArray.count == 3) {
+        NSLog(@"We have a team %@", teamRatingArray);
+        return 1; // Team was formed
+    }
+    for (int i = 0; i < ratingArray.count; ++i) {
+        NSIndexSet* idxSetOfNewArray = [NSIndexSet indexSetWithIndexesInRange: NSMakeRange(i+1, ratingArray.count - (i+1))];
+        if (teamRatingArray.count == 0) {
+            teamFormedCount = teamFormedCount + [self dividePlayersIntoTeams:[ratingArray objectsAtIndexes:idxSetOfNewArray] team:[NSArray arrayWithObjects: ratingArray[i], nil]];
+        } else if (teamRatingArray.count < 2) {
+            teamFormedCount = teamFormedCount + [self dividePlayersIntoTeams:[ratingArray objectsAtIndexes:idxSetOfNewArray] team:[teamRatingArray arrayByAddingObject:ratingArray[i]]];
+        } else if (([ratingArray[i] compare:teamRatingArray[teamRatingArray.count - 1]] == NSOrderedDescending
+                    && [teamRatingArray[teamRatingArray.count - 1] compare:teamRatingArray[teamRatingArray.count - 2]] == NSOrderedDescending)  ||
+                   ([ratingArray[i] compare:teamRatingArray[teamRatingArray.count - 1]] == NSOrderedAscending
+                    && [teamRatingArray[teamRatingArray.count - 1] compare:teamRatingArray[teamRatingArray.count - 2]] == NSOrderedAscending)){
+            NSArray* newTeam = [teamRatingArray arrayByAddingObject:ratingArray[i]];
+            teamFormedCount =  teamFormedCount + [self dividePlayersIntoTeams:[ratingArray objectsAtIndexes:idxSetOfNewArray] team:newTeam];
+        }
+    }
+    return teamFormedCount;
 }
 
 @end
